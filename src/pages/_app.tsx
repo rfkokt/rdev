5389b03dd3370f837c3e89df91c04d45b619d2ab
Rifki Okta <rifkiokta105@gmail.com>
2023-01-21 14:12:30 +0700

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,39 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import RootLayout from '@/components/layouts/Root';
+import WithNavigationFooter from '@/components/layouts/WithNavigationFooter';
+import Provider from '@/providers';
+
+import type { NextPage } from 'next';
+import type { AppProps } from 'next/app';
+import type { ReactElement, ReactNode } from 'react';
+
+import '@/styles/main.css';
+
+type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function getDefaultLayout(page: ReactElement): ReactNode {
+  return <WithNavigationFooter>{page}</WithNavigationFooter>;
 }
+
+function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? getDefaultLayout;
+
+  return (
+      <Provider>
+        <RootLayout>
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          {getLayout(<Component {...pageProps} />)}
+          <Analytics />
+        </RootLayout>
+      </Provider>
+  );
+}
+
+export default App;
